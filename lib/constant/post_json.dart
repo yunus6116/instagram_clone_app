d1cc3a16ed206ed0638dbea517b5575aabfc2ae8
Yunus Kara <ynus.kara6116@gmail.com>
2021-02-08 01:12:43 +0300

    make customizations

---
+++ lib/constant/post_json.dart
@@ -0,0 +1,49 @@
+List posts = [
+  {
+    "id": 1,
+    "name": "kullanci1",
+    "profileImg": "https://picsum.photos/200",
+    "postImg": "https://picsum.photos/200/300",
+    "caption": " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "kullanci4",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 2,
+    "name": "kullanci2",
+    "profileImg": "https://picsum.photos/200/300?grayscale",
+    "postImg": "https://picsum.photos/seed/picsum/200/300",
+    "caption":
+        " ទឹកមុខនៅពេលយើងអត់ចង់ថតរូបនៅកន្លែងសួនផ្កា ហើយគេបង្ខំអោយយើងថត។ 😏😒",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "kullanci3",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 3,
+    "name": "kullanci3",
+    "profileImg":
+        "https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "postImg": "https://picsum.photos/200/300",
+    "caption": " It is a good to day",
+    "isLoved": false,
+    "commentCount": "60",
+    "likedBy": "kullanci1",
+    "timeAgo": "3 day ago"
+  },
+  {
+    "id": 4,
+    "name": "kullanci4",
+    "profileImg": "https://picsum.photos/200",
+    "postImg": "https://picsum.photos/200/300",
+    "caption":
+        "I love how a 6-year-old kid took better photos than many of my friends out there 😂",
+    "isLoved": false,
+    "commentCount": "70",
+    "likedBy": "kullanci2",
+    "timeAgo": "3 day ago"
+  }
+];
---
+++ lib/constant/story_json.dart
@@ -0,0 +1,13 @@
+// user profile
+String profile =
+    "https://media-exp1.licdn.com/dms/image/C4D03AQFQSFA3m8xLfQ/profile-displayphoto-shrink_800_800/0/1583423625748?e=1618444800&v=beta&t=tFLAmnv8fGQbo1rnc4yeBzBawFeWVEso1GytTjqZFIQ";
+String name = "Hikayen";
+
+// stories
+List stories = [
+  {"id": 1, "img": "https://picsum.photos/200", "name": "kullanıcı1"},
+  {"id": 2, "img": "https://picsum.photos/200", "name": "kullanıcı2"},
+  {"id": 3, "img": "https://picsum.photos/200", "name": "kullanıcı3"},
+  {"id": 4, "img": "https://picsum.photos/200", "name": "kullanıcı4"},
+  {"id": 5, "img": "https://picsum.photos/200", "name": "kullanıcı5"}
+];
